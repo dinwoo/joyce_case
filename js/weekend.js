c9aa4e7c3a4bcd12f61a81d783af1feec8301caf
danielWu <danielforboy@gmail.com>
2020-05-24 00:09:25 +0800

    first commit

---
+++ js/weekend.js
@@ -0,0 +1,15 @@
+$('.list').on('click', function () {
+  console.log($(this).data('open'))
+  if (Number($(this).data('open'))) {
+    $(this).data('open', 0)
+    $(this).next().hide();
+    $(this).removeClass('open')
+  } else {
+    $('.list').data('open', 0)
+    $('.audio').hide();
+    $('.list').removeClass('open')
+    $(this).data('open', 1)
+    $(this).next().show();
+    $(this).addClass('open');
+  }
+})
