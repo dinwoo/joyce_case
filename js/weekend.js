e3e400c75923669e8f46fe196ac2616e91d86d50
danielWu <danielforboy@gmail.com>
2020-05-24 00:13:46 +0800

    fix weekend.js

--- js/weekend.js
+++ js/weekend.js
@@ -1,15 +1,16 @@
-$('.list').on('click', function () {
-  console.log($(this).data('open'))
-  if (Number($(this).data('open'))) {
-    $(this).data('open', 0)
-    $(this).next().hide();
-    $(this).removeClass('open')
-  } else {
-    $('.list').data('open', 0)
-    $('.audio').hide();
-    $('.list').removeClass('open')
-    $(this).data('open', 1)
-    $(this).next().show();
-    $(this).addClass('open');
-  }
+$(document).ready(function () {
+  $('.list').on('click', function () {
+    if (Number($(this).data('open'))) {
+      $(this).data('open', 0)
+      $(this).next().hide();
+      $(this).removeClass('open')
+    } else {
+      $('.list').data('open', 0)
+      $('.audio').hide();
+      $('.list').removeClass('open')
+      $(this).data('open', 1)
+      $(this).next().show();
+      $(this).addClass('open');
+    }
+  })
 })
