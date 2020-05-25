afe33bc7e698c607e8b21eadc2d08d9de71ecadb
danielWu <danielforboy@gmail.com>
2020-05-25 21:08:49 +0800

    fix

--- js/weekend.js
+++ js/weekend.js
@@ -1,16 +1,21 @@
 $(document).ready(function () {
-  $('.list').on('click', function () {
-    if (Number($(this).data('open'))) {
-      $(this).data('open', 0)
+  $(".list").on("click", function () {
+    if (Number($(this).data("open"))) {
+      $(this).data("open", 0);
       $(this).next().hide();
-      $(this).removeClass('open')
+      $(this).removeClass("open");
+      $(this).next().find("audio").get(0).pause();
     } else {
-      $('.list').data('open', 0)
-      $('.audio').hide();
-      $('.list').removeClass('open')
-      $(this).data('open', 1)
+      $(".list").data("open", 0);
+      $(".audio").hide();
+      $(".list").removeClass("open");
+      var audioEl = document.querySelectorAll("audio");
+      for (var i = 0; i < audioEl.length; i++) {
+        audioEl[i].pause();
+      }
+      $(this).data("open", 1);
       $(this).next().show();
-      $(this).addClass('open');
+      $(this).addClass("open");
     }
-  })
-})
+  });
+});
